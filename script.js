740def8ae5b203714b0061c12d2934652cef4610
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 12:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
     //Write your code here
-
-
+let strongElement = document.querySelectorAll("strong");
+strongElement.forEach((element)=> element.style.color = "green");
 }
 
 
 function return_normal() {
     //Write your code here
-
+    let strongElement = document.querySelectorAll("strong");
+    strongElement.forEach((element)=> element.style.color = "black");
     
 }
